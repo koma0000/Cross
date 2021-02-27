d693fea7ad9f6f0f19ced94b95fe4f58e9d6d769
Камиль Хаирзаманов <50045075+koma0000@users.noreply.github.com>
2021-02-27 17:47:44 +0300

    LR_1
    Hello MVC

---
+++ khairzamanov_LR1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace khairzamanov_LR1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
