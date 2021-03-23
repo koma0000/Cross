43adce940391a8912fc1cc6d2d48375d46ca9bed
Камиль Хаирзаманов <50045075+koma0000@users.noreply.github.com>
2021-03-23 21:10:46 +0300

    test

---
+++ lab_2/Models/Business.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab_2.Models
+{
+    public class Business
+    {
+        public long CompanyId { get; set; }
+        public Company Companys { get; set; }
+
+    }
+}
---
+++ lab_2/Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab_2.Models
+{
+    public class Company
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long Proceeds { get; set; }
+        public long Profit { get; set; }
+    }
+
+   public List<Business> 
+
+
+   
+}
